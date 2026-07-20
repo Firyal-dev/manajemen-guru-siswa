cde7931d54eba32f0aed2b1c354bc8ae2b48a081
faturahman <faturmriffa@gmail.com>
2026-07-20 07:06:43 +0700

    security: remove stray debug/test scripts

--- test_rombel.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$r = \App\Models\Rombel::with(['kelas', 'kelas.jurusan'])->first();
-echo json_encode($r->toArray(), JSON_PRETTY_PRINT);
